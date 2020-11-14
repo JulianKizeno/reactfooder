63fdeb6ab29663e26be1365f66b0367023609ca5
Julian Kizeno <julianquiceno20@gmail.com>
2020-11-14 20:06:18 +0100

    Updates

---
+++ precache-manifest.af6d65c18e43bdd3d52ccfe4b1f95775.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fc483a54ac9b2afe4370b173abda26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff6db3fc67c7165f4de",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "62d3c55b272d66655b1d",
+    "url": "/static/css/main.bd490989.chunk.css"
+  },
+  {
+    "revision": "7ff6db3fc67c7165f4de",
+    "url": "/static/js/2.6b217338.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6b217338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d3c55b272d66655b1d",
+    "url": "/static/js/main.64b04f99.chunk.js"
+  },
+  {
+    "revision": "18b289a47b81257043fc",
+    "url": "/static/js/runtime-main.2aa1d020.js"
+  },
+  {
+    "revision": "a44bf6ca81fdec6118d0777a71e6615d",
+    "url": "/static/media/background.a44bf6ca.png"
+  },
+  {
+    "revision": "3656f659aa7c6eb1b4cc2aab19e8dbf4",
+    "url": "/static/media/bowlfruit.3656f659.jpg"
+  },
+  {
+    "revision": "bbb0c3e165111601e924216b59301b42",
+    "url": "/static/media/diet.bbb0c3e1.jpg"
+  },
+  {
+    "revision": "cfa807d5c04d24d4efe56fa7ee3509a8",
+    "url": "/static/media/indexbg.cfa807d5.jpg"
+  },
+  {
+    "revision": "7a2791a72a30b63527efcdb901de538f",
+    "url": "/static/media/vegetables1.7a2791a7.jpg"
+  }
+]);
